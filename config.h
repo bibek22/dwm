58dd56a19b4d42e084a8f7e85acc7b8fc6fc57bf
Bibek Gautam <forgot.bg@gmail.com>
2020-06-07 18:35:50 +0545

    added colors and icons to tag

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -12,21 +12,28 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Ubuntu:size=12" };
+static const char *fonts[]          = { "Overpass Nerd Font:size=12" };
 static const char dmenufont[]       = "Ubuntu:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
+static const char col_gray5[]       = "#cccccc";
+static const char col_gray6[]       = "#dddddd";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_purple[]      = "#7c6180";
+static const char col_bluishpurple[] = "#956fd6";
+static const char col_purple_dark[] = "#493f4a";
+static const char col_green_dark[]  = "#005577";
+static const char col_green[]       = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray5, col_purple, col_gray2 },
+	[SchemeSel]  = { col_gray6, col_purple_dark,  col_purple  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
@@ -34,14 +41,15 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class	instance    title       tags mask     iscentered   isfloating   monitor */
-	{ "Gimp",	NULL,       NULL,       0,            0,           1,           -1 },
-	{ "firefox",	NULL,       NULL,       1 << 8,       0,           0,           -1 },
-	{ "ViberPC",	NULL,       NULL,       1 << 7,       0,           0,           -1 },
-	{ "Telegram",	NULL,       NULL,	1 << 7,       0,           0,           -1 },
-	{ "Emacs",	NULL,       NULL,	1 << 2,       0,           0,           -1 },
-	{ "Pavucontrol",NULL,	NULL,       0,	1,           1,           -1 },
-	{ "Zathura",	NULL,	NULL,       1 << 1,0,           0,           -1 },
+	/* class				instance    title       tags mask     iscentered  isfloating   monitor */
+	{ "Gimp",				NULL,				NULL,				0,						0,					1,					-1 },
+	{ "Neomutt",		NULL,				NULL,				1<<2,					0,					1,					-1 },
+	{ "firefox",		NULL,				NULL,				1 << 8,				0,					0,					-1 },
+	{ "ViberPC",		NULL,				NULL,				1 << 7,				0,					0,					-1 },
+	{ "Telegram",		NULL,				NULL,				1 << 7,				0,					0,					-1 },
+	{ "Emacs",			NULL,				NULL,				1 << 2,				0,					0,					-1 },
+	{ "Pavucontrol",NULL,				NULL,				0,						1,					1,					-1 },
+	{ "Zathura",		NULL,				NULL,				1 << 1,				0,					0,					-1 },
 };
 
 /* layout(s) */
