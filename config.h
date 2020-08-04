fe1a03f96ee9f995a6244760af1a783bac600510
Bibek Gautam <forgot.bg@gmail.com>
2020-08-04 20:02:01 +0545

    configs

--- config.h
+++ config.h
@@ -136,7 +136,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_p,      spawn, SHCMD("/usr/local/bin/omnidoer Killall") },
-	/* { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } }, */
+	/* { MODKEY|ShiftMask,            XK_Return, spawn,          {.v = termcmd } }, */
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	/* { MODKEY,                  XK_minus,  removesystrayicon,   {.i = +1} }, */
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -151,7 +151,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	/* { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, */
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,                       XK_t,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,						XK_y,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,             XK_y,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,						XK_f,      togglefullscr,  {0} },
