698d299a61734ffbab00490f1681246acb3ba10f
Bibek Gautam <bibek@hashbang.sh>
2020-08-10 19:05:39 +0545

    black specific

--- dwm.c
+++ dwm.c
@@ -2008,7 +2008,7 @@ setup(void)
 	if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
 	lrpad = 10;   /* padding between tag characters*/
-	bh = drw->fonts->h-2; /* this instead of font height */
+	bh = drw->fonts->h; /* this instead of font height h */
 	updategeom();
 	/* init atoms */
 	utf8string = XInternAtom(dpy, "UTF8_STRING", False);
